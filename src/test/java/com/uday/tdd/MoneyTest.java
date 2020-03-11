0b11e5dbf011d839c1186c79f351d6d99c7e046a
udayk <udaykumar1113@gmail.com>
2020-03-10 21:49:08 -0400

    Testing Dollar and Franc values

--- src/test/java/com/uday/tdd/MoneyTest.java
+++ src/test/java/com/uday/tdd/MoneyTest.java
@@ -10,14 +10,29 @@ public class MoneyTest {
     void testMultiplication(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product);
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
     void testEquality(){
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(7));
+    }
+
+    @Test
+    void testMultiplicationFranc(){
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc(){
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(7));
     }
 }
